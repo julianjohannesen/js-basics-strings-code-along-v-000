399d190db08874a0de2f1919c4734c24903868c2
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-12 17:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let currentUser = "Bob";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
